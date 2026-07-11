69681b3c0ad3e07ff018bd453cdbb30200e04fbd
Olamilekan-oluwayomi <ilesanmiolamilekan05@gmail.com>
2026-07-11 11:21:40 +0100

    feat: initialize App component with lazy-loaded routes and layout structure

--- shore-guesthouse/src/App.jsx
+++ shore-guesthouse/src/App.jsx
@@ -1,22 +1,25 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Gallery from './pages/Gallery'
-import Information from './pages/Information'
-import Layout from './components/Layout'
-import Booking from './pages/Booking'
+import { Suspense, lazy } from "react";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 
+const Home = lazy(() => import("./pages/Home"));
+const Gallery = lazy(() => import("./pages/Gallery"));
+const Information = lazy(() => import("./pages/Information"));
+const Booking = lazy(() => import("./pages/Booking"));
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/information" element={<Information />} />
-        <Route path="/booking" element={<Booking />} />
-      </Route>
-    </Routes>
-  )
+    <Suspense fallback={<div className="flex h-screen items-center justify-center">Loading...</div>}>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/information" element={<Information />} />
+          <Route path="/booking" element={<Booking />} />
+        </Route>
+      </Routes>
+    </Suspense>
+  );
 }
 
-export default App
+export default App;
